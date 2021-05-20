21bc3f9f17e1e258452342a466f3437e10fa7ad8
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-20 21:55:30 +0530

    used banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from './components/Navbar/NavBar';
+import Banner from './components/Banner/Banner';
 import './App.css';
 
 export default function App() {
     return (
         <div>
             <NavBar />
+            <Banner />
         </div>
     )
 }
