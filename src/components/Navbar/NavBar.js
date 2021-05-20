a994c47538d65bb1156fa4da1cfb716a25ab8be9
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-20 21:49:56 +0530

    Navbar

---
+++ src/components/Navbar/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './NavBar.css';
+
+export default function NavBar() {
+    return (
+        <div className="navbar">
+            <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
+                alt="Netflix"
+                className="logo"
+            />
+            <img
+                src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
+                alt="Avatar"
+                className="avatar"
+            />
+        </div>
+    )
+}
