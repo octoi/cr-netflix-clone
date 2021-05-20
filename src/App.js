95fd06a41382c6c8300191af281d8e36b50d790e
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-20 21:42:33 +0530

    used navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import NavBar from './components/Navbar/NavBar';
 
 export default function App() {
     return (
         <div>
-
+            <NavBar />
         </div>
     )
 }
