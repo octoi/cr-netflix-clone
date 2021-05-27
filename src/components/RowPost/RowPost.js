772c31d8f5198cec5bf9b4a3c70b1cf4e6b0ef81
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-27 21:40:35 +0530

    custom data

--- src/components/RowPost/RowPost.js
+++ src/components/RowPost/RowPost.js
@@ -1,24 +1,24 @@
 import { useEffect, useState } from 'react';
-import { apiKey, imgUrl } from '../../constants/constants';
+import { imgUrl } from '../../constants/constants';
 import axios from '../../axios';
 import './RowPost.css';
 
-export default function RowPost() {
+export default function RowPost({ title, small, endpoint }) {
 
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
-        axios.get(`/discover/tv?api_key=${apiKey}&with_networks=213`).then(res => {
+        axios.get(endpoint).then(res => {
             setMovies(res.data.results);
         });
     }, [])
 
     return (
         <div className="row">
-            <h2>Netflix Originals</h2>
+            <h2>{title}</h2>
             <div className="posters">
                 {movies.map((movie, id) => (
-                    <img src={imgUrl + movie.backdrop_path} key={id} alt="poster" className="poster" />
+                    <img src={imgUrl + movie?.backdrop_path} key={id} alt="poster" className={small ? "small-poster" : "poster"} />
                 ))}
             </div>
         </div>
