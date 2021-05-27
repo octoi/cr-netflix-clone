988680e98e3c7fe60c700199c8122d0c3e62a15c
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-27 21:40:32 +0530

    displaying all endpoints

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import NavBar from './components/Navbar/NavBar';
 import Banner from './components/Banner/Banner';
 import RowPost from './components/RowPost/RowPost';
+import * as url from './constants/urls';
 import './App.css';
 
 export default function App() {
@@ -9,7 +10,12 @@ export default function App() {
         <div>
             <NavBar />
             <Banner />
-            <RowPost />
+            <RowPost title="Netflix Originals" endpoint={url.originals} />
+            <RowPost title="Action" small endpoint={url.actions} />
+            <RowPost title="Comedy" small endpoint={url.comedy} />
+            <RowPost title="Action" small endpoint={url.horror} />
+            <RowPost title="Romance" small endpoint={url.romance} />
+            <RowPost title="Documentaries" small endpoint={url.documentaries} />
         </div>
     )
 }
