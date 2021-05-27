236fb1b8c671677e5569b5099b08debabde15f91
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-27 22:08:47 +0530

    displaying trailer

--- src/components/RowPost/RowPost.js
+++ src/components/RowPost/RowPost.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
-import { imgUrl } from '../../constants/constants';
+import { imgUrl, apiKey } from '../../constants/constants';
 import axios from '../../axios';
+import Youtube from 'react-youtube';
 import './RowPost.css';
 
 export default function RowPost({ title, small, endpoint }) {
 
     const [movies, setMovies] = useState([]);
+    const [urlId, setUrlId] = useState('');
 
     useEffect(() => {
         axios.get(endpoint).then(res => {
@@ -13,14 +15,32 @@ export default function RowPost({ title, small, endpoint }) {
         });
     }, [endpoint]);
 
+    const opts = {
+        height: '390',
+        width: '100%',
+        playerVars: {
+            autoplay: 1,
+        }
+    }
+
+    const handleMovie = (id) => {
+        axios.get(`/movie/${id}/videos?api_key=${apiKey}&language=en-US`).then(res => {
+            if (res.data.results.length === 0) throw new Error()
+            setUrlId(res.data.results[0])
+        }).catch(() => {
+            alert("Trailer not available")
+        })
+    }
+
     return (
         <div className="row">
             <h2>{title}</h2>
             <div className="posters">
-                {movies.map((movie, id) => (
-                    <img src={imgUrl + movie?.backdrop_path} key={id} alt="poster" className={small ? "small-poster" : "poster"} />
+                {movies.map(movie => (
+                    <img onClick={() => handleMovie(movie?.id)} src={imgUrl + movie?.backdrop_path} key={movie?.id} alt="poster" className={small ? "small-poster" : "poster"} />
                 ))}
             </div>
+            {urlId && <Youtube opts={opts} videoId={urlId?.key} />}
         </div>
     )
 }
